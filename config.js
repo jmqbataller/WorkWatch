c7a1cef595f6551cc11348e1ba05182daaf20c6d
John Mark <88698057+jmqbataller@users.noreply.github.com>
2026-08-21 20:45:57 +0800

    chore: connect WorkWatch to production Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WORKWATCH_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_PUBLISHABLE_KEY: '',
-  DEMO_MODE: true,
+  SUPABASE_URL: 'https://nuqkaqjaadhjfcqzrsbn.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_23vHr3kBVq-R78VaNhuEfw_I5E021AK',
+  DEMO_MODE: false,
   APP_NAME: 'WorkWatch'
 };
